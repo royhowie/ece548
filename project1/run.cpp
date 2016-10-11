12dfdda700f0b0f3ac37d43ee98e9f391f2aadf0
Roy <roydhowie@gmail.com>
2016-10-11 00:48:13 -0400

    typo

--- project1/run.cpp
+++ project1/run.cpp
@@ -12,11 +12,11 @@ int main () {
     d.scale();
     d.shuffle();
 
-    int epochs = 15;
+    int epochs = 50;
     int order = 5;
 
     for (int i = 1; i <= order; i++) {
         std::cout << "order=" << i << std::endl;
-        Tester(d, i, 0.2).test(30);
+        Tester(d, i, 0.2).test(epochs);
     }
 }
