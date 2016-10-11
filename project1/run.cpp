4833ad656e19a8c3e0da7d25bfe8f8bddbd481e4
Roy Howie <roydhowie@gmail.com>
2016-10-11 12:09:01 -0400

    bug: should pass delimiter to CSVParser

--- project1/run.cpp
+++ project1/run.cpp
@@ -5,15 +5,15 @@
 #include "tester.cpp"
 
 int main () {
-    CSVParser p("data/iris.txt", 4);
-    // CSVParser p("data/skin.txt", 3);
+    //CSVParser p("data/iris.txt", 4);
+    CSVParser p("data/skin.txt", 3, '\t');
     DataSet d(p, 3);
 
     d.scale();
     d.shuffle();
 
     int epochs = 50;
-    int order = 5;
+    int order = 8;
 
     for (int i = 1; i <= order; i++) {
         std::cout << "order=" << i << std::endl;
